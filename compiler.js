c36a1f44d0ae98fcec3890329bba8836c2c0cb44
Kyle Beckles <kyle@arweave.org>
2019-01-15 18:21:50 +0100

    Added nexe webpack wrapper

---
+++ compiler.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const compile = require('nexe').compile;
+
+module.exports = class Compiler {
+
+    constructor(path){
+        this.path = path
+    }
+    apply(compiler) {
+
+        fs.mkdirSync(path.resolve(__dirname, './dist/macos'), {recursive: true})
+        fs.mkdirSync(path.resolve(__dirname, './dist/linux'), {recursive: true})
+        fs.mkdirSync(path.resolve(__dirname, './dist/windows'), {recursive: true})
+
+        compiler.hooks.done.tapAsync(
+            'nexe-build',
+            (compilation, callback) => {
+
+                const builds = [];
+
+                builds.push(compile({
+                    input: this.path,
+                    output: path.resolve(__dirname, './dist/macos/arweave'),
+                    build: false,
+                    targets: [
+                        { version: '10.15.0', platform: 'macos', arch: 'x64' },
+                    ]
+                }));
+
+                builds.push(compile({
+                    input: this.path,
+                    output: path.resolve(__dirname, './dist/linux/arweave'),
+                    build: false,
+                    targets: [
+                        { version: '10.15.0', platform: 'linux', arch: 'x86' },
+                    ]
+                }));
+
+                builds.push(compile({
+                    input: this.path,
+                    output: path.resolve(__dirname, './dist/windows/arweave.exe'),
+                    build: false,
+                    targets: [
+                        { version: '10.15.0', platform: 'windows', arch: 'x86' },
+                    ]
+                }));
+
+                Promise.all(builds).then(() => {callback()});
+            }
+        );
+    }
+}
