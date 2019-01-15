0ea0d5ddde3445972f42adf369be9fd4001875f1
Kyle Beckles <kyle@arweave.org>
2019-01-15 18:59:58 +0100

    Updated nexe version with patched docker support

--- dist/.build/arweave.js
+++ dist/.build/arweave.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54dcdf5ccff2443456f9fcb02a6afab4bf4340d1e8fbf1b9fa17d9d5aa21d906
+oid sha256:33ee11276c933b23268ed5fcf052ce40c69fc4bda11f3cdeab8d02f1f1dc0cc7
 size 1319738
