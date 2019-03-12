2e21550a97d756733dd236caccf11f4f15783410
Kyle Beckles <kyle@arweave.org>
2019-03-12 15:04:44 +0100

    Added remote CSS image importing

--- src/parsers/text-html.ts
+++ src/parsers/text-html.ts
@@ -20,7 +20,7 @@ export class HtmlParser implements ContentParserInterface {
             svgAsImage: true,
             handlers: [
                 (async (source: any, context: any): Promise<any> => {
-                    if (source.type == 'image') {
+                    if (source.type == 'image'){
                         return await importRemoteImages(source, context, logger);
                     }
 
@@ -30,18 +30,14 @@ export class HtmlParser implements ContentParserInterface {
                 })
             ]
         });
-
         return Buffer.from(html, 'utf-8');
     }
+
 }
 
 const importRemoteImages = async (source: any, context: any, logger: Function): Promise<void> => {
     if (source.isRemote && source.type == 'image') {
-
-        const response = await Axios.get(source.sourcepath, {
-            responseType: 'arraybuffer'
-        });
-
+        const response = await downloadRemote(source.sourcepath);
         source.fileContent = response.data;
     }
 }
@@ -64,27 +60,43 @@ const importCssUrls = async (source: any, context: any, logger: Function): Promi
         let path = cssUrl.replace(/^'\//g, '').replace(/('|")/g, '').trim();
 
         // Only import local resources, so don't import https/s externals.
-        if (path.startsWith('data') || path.startsWith('http')) {
+        if (path.startsWith('data:')) {
             continue;
         }
 
-        path = path.replace(/(\#|\?).*/, '');
+        if (path.startsWith('http://') || path.startsWith('https://')) {
+            const response = await downloadRemote(path);
 
-        const resource = new File(path, dirname(source.filepath));
+            output = output.replace(cssUrl, `data:${response.type};base64,${response.data.toString('base64')}`)
+        }else{
+            // For local urls we should remove the # and query strings
+            path = path.replace(/(\#|\?).*/, '');
 
-        if (!await resource.exists()) {
-            throw new Error(`File not found while importing CSS: ${truncate(match[0], 100)}\n\nResource: ${truncate(resource.getPath(), 100)}`);
+            const resource = new File(path, dirname(source.filepath));
+    
+            if (!await resource.exists()) {
+                throw new Error(`File not found while importing CSS: ${truncate(match[0], 100)}\n\nResource: ${truncate(resource.getPath(), 100)}`);
+            }
+    
+            const base64Resource = (await resource.read()).toString('base64');
+    
+            output = output.replace(cssUrl, `data:${resource.getType()};base64,${base64Resource}`)
         }
-
-        const base64Resource = (await resource.read()).toString('base64');
-
-        output = output.replace(cssUrl, `data:${resource.getType()};base64,${base64Resource}`)
     }
 
     source.content = output;
-
 }
 
+async function downloadRemote(url: string): Promise<{data: Buffer, type: string}>{
+    const response = await Axios.get(url, {
+        responseType: 'arraybuffer'
+    });
+
+    return {
+        data: response.data,
+        type: response.headers['content-type']
+    }
+}
 
 function truncate(string: string, length: number){
     return string.length > length ? string.substring(0, 100) + '...' : string;
